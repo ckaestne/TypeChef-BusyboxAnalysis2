46260718e11c4b9ebb750f463b6d79503065f7f3
Christian Kaestner <kaestner@cs.cmu.edu>
2016-06-30 21:51:28 -0400

    remove echo

--- analyze.sh
+++ analyze.sh
@@ -5,10 +5,10 @@ echo WORKDIR: $1
 echo COMMIT: $2
 echo FILE: $3
 
-if [ ! -f run.sh ]; then
-  echo first execution -- compiling tool infrastructure (requires Java and SBT)
-  sbt mkrun
-fi
+#if [ ! -f run.sh ]; then
+#  echo first execution -- compiling tool infrastructure (requires Java and SBT)
+#  sbt mkrun
+#fi
 
 . run.sh  edu.cmu.typechef.busybox.AnalysisFrontend "$1" "$2" "$3"
 
